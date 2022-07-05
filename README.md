25f8695e36442f485f46d57047107dc573214cc8
Wellysson Godinho <wellysson.gomes@gmail.com>
2022-07-05 20:48:54 -0300

    Training react with nextjs[converting to typescript]

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-This is a starter template for [Learn Next.js](https://nextjs.org/learn).# nextjs-blog
+This is a starter template for [Learn Next.js](https://nextjs.org/learn).
 # nextjs-blog
--- components/layout.tsx
+++ components/layout.tsx
@@ -1,13 +1,19 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import Link from 'next/link';
-import utilStyles from '../styles/utils.module.css';
-import styles from './layout.module.css';
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import utilStyles from '../styles/utils.module.css'
+import styles from './layout.module.css'
 
-const name = 'Wellysson Godinho';
-export const siteTitle = 'Next.js Sample Website';
+const name = '[Your Name]'
+export const siteTitle = 'Next.js Sample Website'
 
-export default function Layout({ children, home }) {
+export default function Layout({
+  children,
+  home
+}: {
+  children: React.ReactNode
+  home?: boolean
+}) {
   return (
     <div className={styles.container}>
       <Head>
@@ -19,8 +25,8 @@ export default function Layout({ children, home }) {
         <meta
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle,
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
@@ -69,5 +75,5 @@ export default function Layout({ children, home }) {
         </div>
       )}
     </div>
-  );
+  )
 }
